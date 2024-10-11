0bed8a7489c16d3dabf7100c0b1f8d05fd2e9b3a
Qsen02 <diyangenchev73@gmail.com>
2024-10-11 23:11:59 +0300

    New exercises added

---
+++ Primes 1.0/Primes 1.0/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Primes_1._0
+{ 
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<int> numbers = new ObservableCollection<int>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.primeNumbers.ItemsSource = numbers;
+        }
+        private async void GenerateNumbersHandler(object sender, RoutedEventArgs args) {
+            try
+            {
+                int limit = int.Parse(this.primeLimit.Text);
+                for (int i = 2; i < limit; i++) {
+                    bool isPrime = true;
+                    for (int j = 2; j < i; j++) {
+                        if (i % j == 0) { 
+                        isPrime= false;
+                        }                    
+                    }
+                    if (isPrime) {
+                        numbers.Add(i);
+                    }
+                }
+            }
+            catch (Exception err) {
+                ContentDialog modal = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = "Only integer numbers can be included!",
+                    CloseButtonText = "OK"
+                };
+                await modal.ShowAsync();
+            }
+        }
+    }
+}
---
+++ Primes 2.0/Primes 2.0/Primes 2.0/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Primes_2._0
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void GenerateNumebrsHandler(object sender, EventArgs args)
+        {
+            List<int> primes = new List<int>();
+            int limit = int.Parse(this.primeLimit.Text);
+            for (int k = 2; k < limit; k++)
+            {
+                bool prime = true;
+                for (int j = 2; j < k; j++)
+                {
+                    if (k % j == 0) {
+                        prime = false;
+                     }
+                }
+                if (prime) { 
+                    primes.Add(k); 
+                }
+            }
+            this.primeNumbers.ItemsSource = primes;
+        }
+    }
+}
