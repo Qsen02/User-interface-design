0dc95dcf692cfa61675fafdcb56e2d8647cb9b9c
Qsen02 <diyangenchev73@gmail.com>
2024-10-15 23:15:20 +0300

    New exercise added

---
+++ Course project/Course project/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace Course_project
+{
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<WeatherModel> curWeather=new ObservableCollection<WeatherModel>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Weather.ItemsSource = curWeather;
+        }
+        private async void GetWeatherHandler(object sender, RoutedEventArgs args)
+        {
+            try
+            {
+                string location = this.Location.Text;
+                if (string.IsNullOrEmpty(location)) {
+                    throw new Exception("You must add City and Country separate with comma for location.");
+                }
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync($"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?unitGroup=metric&key=WNMNLKKR9MDRMRCGDNW62GWGB&contentType=json");
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Location don't exist!");
+                }
+                if (curWeather.Count > 0)
+                {
+                    curWeather.Clear();
+                }
+                string json = await response.Content.ReadAsStringAsync();
+                WeatherModel localWeather = JsonConvert.DeserializeObject<WeatherModel>(json);
+                curWeather.Add(localWeather);
+            }
+            catch (Exception err) {
+                ContentDialog modal = new ContentDialog
+                {
+                Title="Error",
+                Content = err.Message,
+                CloseButtonText="OK"
+            };
+                await modal.ShowAsync();
+                return;
+            }
+        }
+    }
+}
---
+++ Course project/Course project/WeatherModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_project
+{
+    public class WeatherModel
+    {
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public Dictionary<string,object> currentConditions { get; set; }
+    }
+}
